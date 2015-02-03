079872559cc02d92cb5c20def8031a3258d8aeb4
U-Babbage_Engine\Manic <basbalmanic@gmail.com>
2015-02-03 10:10:10 -0800

    Updated doxygen tags.

--- Game.cpp
+++ Game.cpp
@@ -1,16 +1,19 @@
-////////////////////////////////////////////////////////////////////////////////
 /*!
-\file   Game.h
-\author Sean McGeer
-\date   1/29/15
-\brief
-  Implement the main Game object.
+  \file   Game.h
+  \author Sean McGeer
+  \date   1/29/15
+  \brief
+    Implement the main Game object.
 */
-////////////////////////////////////////////////////////////////////////////////
-
 #include "Game.h"
 #include <iostream>
 
+/*!
+  Creates a new game object.
+  
+  \param FrameRate
+    The framerate in FPS to set the game framerate to.
+*/
 Game::Game(int FrameRate)
 {
   FrameRate_ = FrameRate;
@@ -21,6 +24,12 @@ Game::~Game()
   
 }
 
+/*!
+  Runs the game.
+  
+  \return
+    An integer code to return to the OS.
+*/
 int Game::Run()
 {
   std::cout << "Hello, world!  Framerate set to: " << FrameRate_ << std::endl;
--- Game.h
+++ Game.h
@@ -1,13 +1,18 @@
-////////////////////////////////////////////////////////////////////////////////
-/*!
-\file     Game.h
-\author   Sean McGeer
-\date     1/29/15
-\brief
-  Define the main Game object.
-*/
-////////////////////////////////////////////////////////////////////////////////
 
+/*!
+  \file     Game.h
+  \author   Sean McGeer
+  \date     1/29/15
+  \brief
+    Define the main Game object.
+*/
+
+/*!
+  \class Game
+  \brief  
+    The game class implements a game object which loads all other modules and
+    handles the main running of the game.
+*/
 class Game
 {
   public:
